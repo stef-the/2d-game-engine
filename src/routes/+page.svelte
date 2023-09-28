e9d3cb681f7735dae3d921426b7bbd805909af5f
stef <stefthedoggo4@gmail.com>
2023-09-28 18:32:49 +0100

    add debug menu + freeze state and tick information

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,47 @@
 <script>
   import { controls, world, c } from "../lib/data.js";
-  import { onMount, tick } from "svelte";
+  import { onMount } from "svelte";
 
-  // Set player position
+  // Set player position to 0,0
   let pos = {
     x: 0,
     y: 0,
   };
 
-  // Set screen ratio (Allows for responsive design)
-  let screenratio = 50;
+  /* 
+    Set game variables
+  */
+  let screenratio = 50; // Set screen ratio (Allows for responsive design)
+  const debugMenuOpacityDefault = 70; // Set default debug menu opacity
+  let debugMenuOpacity = debugMenuOpacityDefault; // Prepare debug menu show and hide
 
-  // Set tickrate
-  const tickrate = 10;
+  let lastTick = new Date().getTime(); // Set last tick time
+  let lastTickDifference = new Date().getTime() - lastTick; // Set last tick difference
+  let lastTickList = []; // Set last tick list
+  let avgTickRate = 0; // Set average tick rate
+  let totalTicks = 0; // Set total ticks
 
-  // Store and process value for keys pressed
-  let keysDown = [];
+  const tickrate = 10; // Set update tickrate (ms) -> time in between updates
 
+  let notFrozen = true; // Set freeze state
+
+  function toggleDebugMenu(e) {
+    if (e == controls.debugMenu) {
+      if (debugMenuOpacity === 0) {
+        debugMenuOpacity = debugMenuOpacityDefault;
+      } else {
+        debugMenuOpacity = 0;
+      }
+    }
+  }
+
+  /*
+    Handle interactions with the user
+  */
+  let keysDown = []; // Store and process value for keys pressed
+  let buttonsOver = []; // Store and process value for buttons pressed
+
+  // Handle keys and buttons pressed
   function keyDownHandler(e) {
     if (!keysDown.includes(e)) {
       keysDown.push(e);
@@ -25,10 +50,6 @@
   function keyUpHandler(e) {
     keysDown.splice(keysDown.indexOf(e), 1);
   }
-
-  // Store and process value for buttons pressed
-  let buttonsOver = [];
-
   function mouseOverBtn(e) {
     if (!buttonsOver.includes(e.target.id)) {
       buttonsOver.push(e.target.id);
@@ -40,18 +61,28 @@
 
   // Handle game updates
   function update() {
-    // Handle player movement
-    if (keysDown.includes(controls.moveUp)) {
-      pos.y -= 1;
+    lastTickDifference = new Date().getTime() - lastTick; // Calculate last tick difference
+    lastTick = new Date().getTime(); // Update last tick time
+    lastTickList.push(lastTickDifference); // Add last tick difference to list
+    if (totalTicks > 10) {
+      lastTickList.shift(); // Remove first item from list
     }
-    if (keysDown.includes(controls.moveDown)) {
-      pos.y += 1;
-    }
-    if (keysDown.includes(controls.moveLeft)) {
-      pos.x -= 1;
-    }
-    if (keysDown.includes(controls.moveRight)) {
-      pos.x += 1;
+    totalTicks++; // Increment total ticks
+    avgTickRate = Math.round(lastTickList.reduce((a, b) => a + b, 0) / lastTickList.length); // Calculate average tick rate
+    if (notFrozen) {
+      // Handle player movement
+      if (keysDown.includes(controls.moveUp)) {
+        pos.y -= 1;
+      }
+      if (keysDown.includes(controls.moveDown)) {
+        pos.y += 1;
+      }
+      if (keysDown.includes(controls.moveLeft)) {
+        pos.x -= 1;
+      }
+      if (keysDown.includes(controls.moveRight)) {
+        pos.x += 1;
+      }
     }
   }
 
@@ -90,6 +121,9 @@
   on:keydown={(e) => {
     // Handles on key down event
     keyDownHandler(e.key);
+
+    // Handles event for single-time actions
+    toggleDebugMenu(e.key);
   }}
   on:keyup={(e) => {
     // Handles on key up event
@@ -136,9 +170,29 @@
       }
     }
   }}
-  use:cssVariables={{ screenratio }}
+  use:cssVariables={{ screenratio, debugMenuOpacity }}
 />
 
+<!-- Render world first -->
+<div
+  id="world"
+  style="top: {pos.y * (screenratio / -20)}px; left: {pos.x *
+    (screenratio / -20)}px;"
+>
+  {#each world as world_column}
+    {#each world_column as world_element}
+      <div
+        class="block"
+        style="
+        background-color: {world_element};
+        top: {world.indexOf(world_column) * screenratio}px;
+        left: {world_column.indexOf(world_element) * screenratio}px;"
+      />
+    {/each}
+  {/each}
+</div>
+
+<!-- Render buttons -->
 <div class="buttonContainer">
   <div id="directionalButtons" class="buttonContainer">
     <button
@@ -179,28 +233,28 @@
   </div>
 </div>
 
+<!-- Render player -->
 <div id="player" />
 
-<!-- Render world -->
-<div
-  id="world"
-  style="top: {pos.y * (screenratio / -20)}px; left: {pos.x *
-    (screenratio / -20)}px;"
->
-  {#each world as world_column}
-    {#each world_column as world_element}
-      <div
-        class="block"
-        style="
-        background-color: {world_element};
-        top: {world.indexOf(world_column) * screenratio}px;
-        left: {world_column.indexOf(world_element) * screenratio}px;"
-      />
-    {/each}
-  {/each}
+<!-- Render debug info -->
+<div id="debug">
+  <span>coordinates (x, y): <span>{pos.x}, {pos.y}</span></span><br />
+  <span>last tick: {lastTickDifference}ms</span><br />
+  <span>total ticks: {totalTicks}</span><br />
+  <span>average tick rate: {avgTickRate}ms</span><br />
 </div>
 
 <style>
+  #debug {
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: darkgrey;
+    color: darkblue;
+    opacity: calc(var(--debugMenuOpacity) * 1%);
+    font-size: small;
+  }
+
   #world {
     position: fixed;
   }
